bb39ca7512487c993c12eceb99dfaaf6d01d4b49
vivangkumar <vivangkumar@gmail.com>
2014-12-02 15:51:28 +0000

    Add more tests

--- test/CollectionTest.js
+++ test/CollectionTest.js
@@ -17,17 +17,27 @@
      describe('Empty Collection', function() {
          it('should create a new collection with no elements which is an instance of Collection', function() {
              expect(emptyCollection).to.be.an.instanceof(Collection);
-         })
+         });
      });
      describe('Collection with elements', function() {
-         it('should create a new collection with 3 elements and an instance of Collection', function() {
+         it('should create a new collection which is an instance of Collection', function() {
              expect(collectionWithElements).to.be.an.instanceof(Collection);
+         });
+         it('should have 3 elements', function() {
              collectionWithElements.elements.should.have.length(3);
          })
      });
      describe('Show collection', function() {
          it('should show all elements of a collection', function() {
              expect(collectionWithElements.show()).to.eql(["a", "b", "c"]);
+         });
+         it('should have 3 elements', function() {
+             collectionWithElements.elements.should.have.length(3);
+         });
+     });
+     describe('Collection size', function() {
+         it('should return a size of 3', function() {
+             collectionWithElements.size().should.equal(3);
          })
      })
  })
