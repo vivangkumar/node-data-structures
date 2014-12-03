93eab22572ca0c587d1c5417e1af0304f47811ab
Vivan <vivangkumar@gmail.com>
2014-12-03 00:07:01 +0000

    Add more tests

--- test/CollectionTest.js
+++ test/CollectionTest.js
@@ -14,11 +14,13 @@
  describe('Collection', function() {
      var emptyCollection = new Collection();
      var collectionWithElements = new Collection("a", "b", "c");
+
      describe('Empty Collection', function() {
          it('should create a new collection with no elements which is an instance of Collection', function() {
              expect(emptyCollection).to.be.an.instanceof(Collection);
          });
      });
+
      describe('Collection with elements', function() {
          it('should create a new collection which is an instance of Collection', function() {
              expect(collectionWithElements).to.be.an.instanceof(Collection);
@@ -27,6 +29,7 @@
              collectionWithElements.elements.should.have.length(3);
          })
      });
+
      describe('Show collection', function() {
          it('should show all elements of a collection', function() {
              expect(collectionWithElements.show()).to.eql(["a", "b", "c"]);
@@ -35,9 +38,25 @@
              collectionWithElements.elements.should.have.length(3);
          });
      });
+
      describe('Collection size', function() {
          it('should return a size of 3', function() {
              collectionWithElements.size().should.equal(3);
          })
-     })
- })
+     });
+
+     describe('Add', function() {
+         it('should add a new element to the collection', function() {
+             assert.isTrue(emptyCollection.add("x"));
+         });
+         it('should have a length 1', function() {
+            emptyCollection.elements.should.have.length(1);
+         });
+         it('should have one element "x"', function() {
+             expect(emptyCollection.show()).to.eql(["x"]);
+         });
+         it('should return false if no arguments are present', function() {
+             assert.isFalse(emptyCollection.add());
+         });
+     });
+ });
